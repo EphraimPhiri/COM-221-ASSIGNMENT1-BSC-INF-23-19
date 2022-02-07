9dbfc49b2c86cc5af5c1582f9506d42f775d8849
EphraimPhiri <bsc-inf-23-19@unima.ac.mw>
2022-02-07 12:57:17 -0800

    restructured the super class

--- Postgraduate.java
+++ Postgraduate.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 public class Postgraduate extends StudentLoanAccount{
     private double researchGrantLoan;
     Scanner input = new Scanner(System.in);
@@ -6,30 +6,29 @@ public class Postgraduate extends StudentLoanAccount{
     Postgraduate(){
 
     }
-    Postgraduate(String name,String programEnrolled,int currentYear,double tuition,double subsistence,double researchGrantLoan){
-        while(researchGrantLoan!=0&&(researchGrantLoan<50000 || researchGrantLoan >100000)){
-            System.out.println("Amount entered for research grant should be within 50000 and 100000.");
-           researchGrantLoan = input.nextDouble();
-        }
-        this.researchGrantLoan = researchGrantLoan;
+    Postgraduate(String name,String programEnrolled,int currentYear){
+        super(name,ProgramEnrolled,CurrentYear,0.15,3,"PG");//prepend for postgraduate is PG, subsistence loan interest is 15% and duration is 3 years
     }
-
-    public void setResearchGrantLoan(double researchGrantLoan){
-        while(researchGrantLoan!=0&&(researchGrantLoan<50000 || researchGrantLoan >100000)){
-            System.out.println("Amount entered for research grant should be within 50000 and 100000.");
-           researchGrantLoan = input.nextDouble();
+    public void applyResearchGrantLoan(double researchGrantLoan){
+        //loop to ensure that user enters a value within the range
+        while(researchGrantLoan<50000 || researchGrantLoan >100000){
+            System.out.print("\nSorry, amount entered for research grant should be within 50000 and 100000. \n Enter amount within the given range:");
+            researchGrantLoan = input.nextDouble();
         }
-        this.researchGrantLoan = researchGrantLoan;
+         this.researchGrantLoan += researchGrantLoan ;     
+           System.out.println("___Research-grant granted___");
     }
-
     public double getResearchGrantLoan(){
-        return researchGrantLoan;
+        return this.researchGrantLoan;
     }
-    public void applyForSubsistenceLoan(double amount) {
-        double this.amount= amount;
-        super.applyForSubsistenceLoan(amount, .15);
+    @Override
+    public void getAllLoans() {
+        //when user has no loanS
+        if(super.getTuitionLoan() ==0 && super.getSubsistenceLoan() ==0 && this.researchGrantLoan == 0 ){
+            System.out.println("\nYou don't have any Loan at the moment.");
+            return;
+        }
+        super.getAllLoans();
+        System.out.printf("\nResearch     \t\t %12.2f \t\t %9s",this.researchGrantLoan," Not applicable \n" );
     }
-    // public String toString(){
-    //     return "Account name "+ getName() + "\tprogram name" + getProgramEnrolled() + "\tAccount Number PG" + getAccNumber() + "\n";
-    // }
 }
--- StudentLoanAccount.java
+++ StudentLoanAccount.java
@@ -1,72 +1,191 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class StudentLoanAccount{
+    public static ArrayList<StudentLoanAccount> loanAccounts = new ArrayList<>();
     private String name;
     private String programEnrolled;
     private int currentYear;
-    private double tuition;
-    private double subsistence;
     private int programDuration;
-    private double tuitionLoanInterest;
-    private double subsistenceLoanInterest;
+    private double tuition;
+    private double tuitionInterest;
+    private double subsistence;
+    private double subsistenceInterest;
+    private String accountNumber;
+    public final double subsistenceInterestRate;
+
     StudentLoanAccount(){
 
     }
-    StudentLoanAccount(String name,String programEnrolled,int currentYear,double tuition,double subsistence,int programDuration){
+    public StudentLoanAccount(String name,String programEnrolled, int currentYear, double subsistenceInterestRate, int programDuration, String accountNumberCode){
         this.name = name;
         this.programEnrolled = programEnrolled;
         this.currentYear = currentYear;
-        this.tuition = tuition;
-        this.subsistence = subsistence;
         this.programDuration = programDuration;
+        this.subsistenceInterestRate = subsistenceInterestRate;
+        //generate an account number using the method generateAccountNumber and assigning it to the variable 
+        this.accountNumber = generateAccountNumber(accountNumberCode);
     }
-    public String getName(){
+    // method to generate an  account Number for a particular Student
+    private String generateAccountNumber(String prepend){
+        Random random = new Random(3);
+        String accountNum;
+        //generates an account number without checking the array
+        if((StudentLoanAccount.loanAccounts).isEmpty()){
+            accountNum = prepend + random.nextInt(1000);
+            return accountNum;
+        }
+        //compares the account number generated with other account numbers generated
+        else{
+            Boolean accountExists = true;
+            //iterates until account number generated is unique
+            do{
+                //generates account number before comparing
+                accountNum = prepend + random.nextInt(1000);
+                //comparing operation
+                for(int i = 0; i< (StudentLoanAccount.loanAccounts).size(); i++){
+                    
+                    if(StudentLoanAccount.loanAccounts.get(i).getAccountNumber().equals(accountNum)){
+                        accountExists = true;
+                        break;  //breaks the for loop to generate another number in do while since the account exists
+                    }
+                    //Reaching the last index implies the account number does not exists therefore use the account Number generated
+                    if(i+1 == StudentLoanAccount.loanAccounts.size()){
+                        accountExists = false;
+                    }
+                }
+
+            }while(accountExists);
+            return accountNum;
+        }
+    }
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public String getName() {
         return name;
     }
-    public String getProgramEnrolled(){
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getProgramEnrolled() {
         return programEnrolled;
     }
-    public int getCurrentYear(){
+
+    public void setProgramEnrolled(String programEnrolled) {
+       this.programEnrolled = programEnrolled;
+    }
+
+    public int getCurrentYear() {
         return currentYear;
     }
-    public double getTuition(){
-        return tuition;
+
+    public void setCurrentYear(int currentYear) {
+        this.currentYear = currentYear;
     }
-    public double getSubsistence(){
-        return subsistence;
-    }
-    public int getProgramDuration(){
+
+    public int getProgramDuration() {
         return programDuration;
     }
-    public void setName(String name){
-        this.name=name;
-    }
-    public void setProgramEnrolled(String programEnrolled){
-        this.programEnrolled=programEnrolled;
-    }
-    public void setCurrentYear(int currentYear){
-        this.currentYear=currentYear;
-    }
-    public void setTuition(double tuition){
-        this.tuition=tuition;
-    }
-    public void setSubsistence(double subsistence){
-        this.subsistence=subsistence;
-    }
-    public void setProgramDuration(int programDuration){
+
+    public void setProgramDuration(int programDuration) {
         this.programDuration = programDuration;
     }
+    // sum up all the loans
+     public double getTotalLoans(){
+        return this.tuition + this.subsistence;
+    }
+
     public void applyForTuitionLoan(double amount){
-        double repayAmount = AnnualCompoundInterest(amount, 0.1);
-        this.tuitionLoan += repayAmount; 
-        this.tuitionLoanInterest = repayAmount - amount;
+        /** 
+         the amount to be applied is specified in the  method parameters and using the Annual Compound interest the amount to be paid back
+         is calculated and passed to the private field Tuition and the interest is calculated by subtracting 
+         the amount to be repaid minus the amount applied
+        */
+        double amountToBePaid = AnnualCompoundInterest(amount, 0.10);
+        this.tuition += amountToBePaid;
+        this.tuitionInterest = amountToBePaid - amount;
+        System.out.println("___Tuition Loan Applied successfully___");
     }
-    public void applyForSubsistenceLoan(double amount, double rate){
-        double repayAmount = AnnualCompoundInterest(amount, rate);
-        this.subsistenceLoan += repayAmount;
-        this.subsistenceLoanInterest = repayAmount - amount;
+    public void repayTuitionLoan(double amount){
+        if(this.tuition <= 0){
+            System.out.printf("You don't have any tuition loan right now...");
+            return;
+        }
+        double extra = 0 ; //Amount left after paying the loan
+        //message when the amount paid fully repays the loan 
+        if(this.tuition - amount <= 0){
+            System.out.println("You've Successfully repaid your tuition loan in full ");
+            
+            //returns Money left when amount is greater than the specified loan
+            if(this.tuition - amount < 0) {
+                extra = amount - this.tuition;
+                System.out.printf("The money left is : MWK%.2f returned \n", extra );
+            }
+            //When User has no Tuition Loan
+            this.tuition = 0;
+            this.TuitionLoanInterest = 0; //sets interest to zero when the loan is fully repaid
+        }
+        //message when the amount paid does not fully repay the loan 
+        else{ 
+            System.out.printf("You've paid MWK%.2f to your tuition loan\n", amount);
+            this.tuition -= amount;
+        }     
     }
-    public double AnnualCompoundInterest(double amount, double rate) {
-        int time =  this.programDuration - this.currentYear;
-        double A = amount * (Math.pow((1 + rate), time ));
-        return A;
+    public double getTuitionLoan(){
+        return this.tuition;
+    }
+    public void applyForSubsistenceLoan(double amount){
+        /** 
+         the amount to be applied is specified in the  method parameters and using the Annual Compound interest. the amount to be paid back
+         is calculated and passed to the private field Subsistence and the interest is calculated by subtracting 
+         the amount to be repaid minus the amount applied
+        */
+        double amountToBePaid = AnnualCompoundInterest(amount, this.subsistenceInterestRate);
+        this.subsistence += amountToBePaid;
+        this.subsistenceInterest = amountToBePaid - amount;
+        System.out.println("___ Subsistence loan Applied successfully ___");
+    }
+    public void repaySubsistenceLoan(double amount){
+            //returns amount paid when user has no loans
+            if(this.subsistence <= 0){
+                System.out.printf("You currently do not have any subsistence loan..." + 
+                                "\nMWK%.2f returned \n", amount );
+                return;
+            }
+        double extra = 0 ; //money left after paying the loan
+        if(this.subsistence - amount <= 0){
+            System.out.println("You've Successfully repaid your subsistence loan.");
+            //returns extra money if the amount paid back is Greater than the loan 
+            if(this.subsistence - amount < 0) {
+                extra = amount - this.subsistence;
+                System.out.printf("The extra money: MWK%.2f returned \n", extra );
+            }
+            //When User has no loans...
+            this.subsistence = 0;
+            this.subsistenceInterest = 0;//sets interest to zero when the loan is fully repaid
+        }
+        else{ 
+            System.out.printf("You've paid MWK%.2f to your subsistence loan\n", amount);
+            this.subsistence -= amount;
+        }
+    }
+    public double getSubsistenceLoan(){
+        return this.tuition;
+    }
+    //method to display all loans available  alongside their interests
+    public void getAllLoans(){
+        System.out.println("Type of Loan \t\t Loan amount\t\t Interest added" + 
+        "\n------------------------------------------------------------------------------");
+        System.out.printf("Tuition      \t\t %12.2f \t\t %9.2f" , this.tuition, this.tuitionInterest);
+        System.out.printf("\nSubsistence  \t\t %12.2f \t\t %9.2f", this.subsistence, this.subsistenceInterest);
+    }
+    public double AnnualCompoundInterest(double amount, double rate){
+        int numOfYears = this.programDuration - this.currentYear;
+        double ACI = amount * ( Math.pow((1 + rate), numOfYears));
+        return ACI;
     }
 }
